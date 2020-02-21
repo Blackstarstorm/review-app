0ff73f9a51b07fa159e9b8424055b6688152765f
Winston Meikle <winmeikle@gmail.com>
2020-02-20 20:00:54 -0500

    learning more

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
+      <Card>
       <Text>{navigation.gotParam('title')}</Text>
       <Text>{navigation.gotParam('body')}</Text>
       <Text>{navigation.gotParam('rating')}</Text>
+     </Card>
     </View>
   )
 }
